4c6d93467f3bbcafbc4e51cb39030e3a33bb0ce2
tga-business <81005306+tga-business@users.noreply.github.com>
2021-04-27 21:30:59 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC")
   end
-
-  def new
-  end
+#DESCでidの降順に並び替え
+  #def new
+  #end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index 
   end
+
+
 end
